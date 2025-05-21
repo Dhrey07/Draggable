4ba12a02b478a9ff3de631a36618c14cf51113f0
Damilare Joseph Oyebamiji <datamayor75@gmail.com>
2025-05-21 20:15:52 +0100

    PATCH: name correction

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import DraggableCard from './DraggableCards';
 const cardData = [
   { id: 1, title: "Card One", content: "My Card.", position: { x: 50, y: 100 } },
   { id: 2, title: "Card Two", content: "Mr Azeez card.", position: { x: 300, y: 150 } },
-  { id: 3, title: "Card Three", content: "Madame Funmi's card.", position: { x: 550, y: 200 } },
+  { id: 3, title: "Card Three", content: "Madame Titilayo card.", position: { x: 550, y: 200 } },
 ];
 
 const App = () => {
